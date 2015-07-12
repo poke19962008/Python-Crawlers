ecdaa3516287c57733fe95b5b48022663b544967
Sayan Das <poke19962008@gmail.com>
2015-07-12 23:19:47 +0530

    Bug fixes : http://www.buyingiq.com/

--- Electronics/Retrieval/retrieval.py
+++ Electronics/Retrieval/retrieval.py
@@ -1,12 +1,13 @@
 __author__ = 'poke19962008'
 
 '''
-    what is the features of Vostro 14 V3446?
-    what is the features of Vostro 14 V3446 Notebook?
-    what is the feature of Vostro 14 V3446?
+    what are the features of Vostro 14 V3446?
+    what are the features of Vostro 14 V3446 Notebook?
+    what are the feature of Vostro 14 V3446?
+    what are the features of Dell Vostro? <---- Return multiple matching results
 '''
 
-import pymongo, json, sys
+import pymongo, json
 import keywordExtractor
 key = ['feature', 'features', 'type', 'company', 'brand']
 
@@ -17,8 +18,7 @@ def main():
         print("Connection not successful ")
     dbh = con["electronics"]
 
-    # string = " ".join(sys.argv[1:])
-    query = keywordExtractor.extract("what is the features of Vostro 14 V3446?")
+    query = keywordExtractor.extract("what are the features of Samsung Galaxy camera?")
     what = [x for x in query if x in key]
     query.remove(what[0])
     model = " ".join(query)
@@ -30,7 +30,21 @@ def main():
     elif what in ["company", 'brand']:
         what = "company"
 
-    tuple = dbh.users.find_one({
+    company = dbh.users.find_one({
+                                "company": {
+                                    "$regex": model.split()[0],
+                                    "$options": "i"
+                                }
+                            }
+                        ,  {
+                                "company": True
+                            }
+    )
+
+    if company != None:
+        model = " ".join(model.split()[1:])
+
+    tuple = dbh.users.find({
                                     "model": {
                                         "$regex": model,
                                         "$options": "i"
@@ -39,11 +53,14 @@ def main():
                                , {
                                     "_id": False,
                                     "model": True,
+                                    "company": True,
                                     what: True,
                                     "img src": True,
                                     "website link": True
                                   })
-    print(tuple)
+    print("Results Found: " , tuple.count())
+    for i in range(tuple.count()):
+        print(tuple[i])
 
 if __name__ == "__main__":
     main()
